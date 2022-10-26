b36075c36a4f8364156a224f8cc4e1ae150d99b4
Kire Kutanovski <kire.kutanovski@cns.uist.edu.mk>
2022-10-26 22:22:11 +0200

    latest

---
+++ script.js
@@ -0,0 +1,17 @@
+ScrollReveal({
+    reset: false,
+    distance: '60px',
+    duration: 1000,
+    delay: 400
+  });
+
+  ScrollReveal().reveal('.nav-text', {delay: 400, origin: 'right', reset: false});
+  ScrollReveal().reveal('.building', {delay: 500, origin: 'top'});
+  ScrollReveal().reveal('.txt1', {delay: 600, origin: 'right'});
+  ScrollReveal().reveal('.train', {delay: 500, origin: 'top'});
+  ScrollReveal().reveal('.txt2', {delay: 600, origin: 'left'});
+  ScrollReveal().reveal('.lights, .footer-text', {delay: 500, origin: 'top'});
+  ScrollReveal().reveal('.txt3', {delay: 600, origin: 'right'});
+  ScrollReveal().reveal('.txt3-1, .txt1-1, .txt1-2', {delay: 700, origin: 'bottom'});
+  ScrollReveal().reveal('.icons', {delay: 800, origin: 'right'});
+  ScrollReveal().reveal('.arrow', {delay: 800, origin: 'bottom'});
